6d1bb6cf3a07823fe8550568e09c324f9051a3a6
darkfateak07 <155147970+darkfateak07@users.noreply.github.com>
2025-07-28 11:55:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Counter from './components/Counter'
-import ExpenseContainer from './components/expense-container'
+import ExpenseContainer from './components/Expense-container'
 
 const App=()=> {
   return (
